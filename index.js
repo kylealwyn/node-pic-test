977c1a6da8e648b2f2211d4bcd128ed656b4e296
Kyle Alwyn <kyle@lemonaid.com>
2016-04-20 16:39:17 -0700

    add body pareser

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ var http = require('http');
 var express = require('express');
 var multer  = require('multer')
 var upload = multer({ dest: 'uploads/' });
+var bodyParser = require('body-parser');
 
 var app = express();
-
+// Decode JSON
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.post('/', upload.single('11'), function (req, res) {
   console.log(req);
 });
